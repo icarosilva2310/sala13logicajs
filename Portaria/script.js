86056367524df17a1ff3d03e7d46dc0003e2477f
icarosilva2310 <icarocardozosilva@gmail.com>
2026-05-07 11:58:16 -0300

    Add files via upload

---
+++ Portaria/script.js
@@ -0,0 +1,26 @@
+function check(){
+    var wasBorn = parseInt(document.getElementById('birth').value)
+    var res = document.getElementById('result')
+    var clientAge = document.getElementById('age')
+
+    if(isNaN(wasBorn)){
+        res.textContent = "Type only numbers 🚫"
+        res.style.color = 'red'
+        return
+    }
+
+    const thisYear = new Date().getFullYear()
+    const age = thisYear - wasBorn
+
+    if(age >= 18){
+        res.textContent = "Over 18, ENTRY ✅"
+        clientAge.textContent = age
+        document.getElementById('v1').style.display = 'block'
+        document.getElementById('v2').style.display = 'none'
+    }else{
+        res.textContent = "Under 18, NOT ALLOWED 🔞"
+        clientAge.textContent = age
+        document.getElementById('v2').style.display = 'block'
+        document.getElementById('v1').style.display = 'none'
+    }
+}
